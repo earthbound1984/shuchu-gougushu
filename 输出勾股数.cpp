2e394b38b39ee2cff0460e9a8c1cce31dd546756
earthbound1984 <91579482+earthbound1984@users.noreply.github.com>
2022-05-09 21:22:46 +0800

    Add files via upload

---
+++ 输出勾股数.cpp
@@ -0,0 +1,17 @@
+﻿
+
+#include <stdio.h>
+
+int main()
+{
+	int a, b, c,i,n;
+for(a=1;a<=100;a++)
+for(b=a+1;b<=100;b++)
+for (c = b+1; c <= 100; c++)
+{
+	if (a * a + b * b == c * c)
+		printf("%2d %2d %2d\n", a, b, c);
+}
+	
+}
+
